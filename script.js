5b55fa704fc7e96fddac3224a437134ecb05e607
Kyle Mitofsky <kmitofsky@dealerpolicy.com>
2021-09-30 11:36:36 -0400

    add storage gen

---
+++ script.js
@@ -0,0 +1,31 @@
+let counter = 0;
+let debugEl = document.getElementById("dump")
+
+setInterval(function() {
+    setRandomSessionStorage()
+    displaySessionStorage()
+}, 500)
+
+function setRandomSessionStorage() {
+    const random = getRandomString(1000)
+    window.sessionStorage.setItem(counter.toString(), random)
+    counter++
+}
+
+function displaySessionStorage() {
+    const obj = getSessionStorage()
+    const str = JSON.stringify(obj, null, 2)
+    debugEl.innerText = str;
+
+}
+
+function getSessionStorage() {
+    return Object.keys(sessionStorage).reduce(function(obj, key) {
+        obj[key] = sessionStorage.getItem(key).substring(0, 5) + '...';
+        return obj
+    }, {});
+}
+
+function getRandomString(len) {
+    return new Array(len).join().replace(/(.|$)/g, function(){return ((Math.random()*36)|0).toString(36);})
+}
