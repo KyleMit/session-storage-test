dafd3462103760d865e075d9cf9a33bef0b9a447
Kyle Mitofsky <kmitofsky@dealerpolicy.com>
2021-09-30 16:33:48 -0400

    add info and controls

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 let counter = 0;
-let debugEl = document.getElementById("dump")
+let dumpEl = document.getElementById("dump")
+let sizeEl = document.getElementById("size")
+let lengthEl = document.getElementById("length")
+let intervalEl = document.getElementById("interval")
 
-setInterval(function() {
-    setRandomSessionStorage()
-    displaySessionStorage()
-}, 50)
+main()
+
+function main() {
+    setTimeout(function() {
+        setRandomSessionStorage()
+        displaySessionStorage()
+        displaySize()
+
+        // rerun
+        if (Number(intervalEl.value) > 0) main()
+    }, intervalEl.value)
+}
 
 function setRandomSessionStorage() {
-    const random = getRandomString(5000)
+    const random = getRandomString(lengthEl.value)
     window.sessionStorage.setItem(counter.toString(), random)
     counter++
+}
+
+function getRandomString(len) {
+    return new Array(len).join().replace(/(.|$)/g, function(){return ((Math.random()*36)|0).toString(36);})
 }
 
 function displaySessionStorage() {
     const obj = getSessionStorage()
     const str = JSON.stringify(obj, null, 2)
-    debugEl.innerText = str;
+    dumpEl.innerText = str;
 
 }
 
@@ -26,6 +41,17 @@ function getSessionStorage() {
     }, {});
 }
 
-function getRandomString(len) {
-    return new Array(len).join().replace(/(.|$)/g, function(){return ((Math.random()*36)|0).toString(36);})
+
+function displaySize() {
+    const keys = Object.keys(sessionStorage)
+    const item = sessionStorage.getItem(keys[0])
+    const bytes = keys.length * item.length * 2
+
+    if (bytes / 1024 < 1) {
+        sizeEl.innerText = bytes.toFixed(2) + " bytes"
+    } else if (bytes / 1024 / 1024 < 1) {
+        sizeEl.innerText = (bytes / 1024).toFixed(2) + " kb"
+    } else {
+        sizeEl.innerText = (bytes / 1024 / 1024).toFixed(2) + " mb"
+    }
 }
